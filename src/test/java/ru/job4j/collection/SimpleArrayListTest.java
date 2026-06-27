1564cc7485dd52e64dcf9f76f12ef7b1c474c0fc
nova <novopashinva@gmail.com>
2026-06-27 22:31:56 +0700

    fix:Отключил тесты, пока нет реализации 1. Динамический список на массиве. [#158]

--- src/test/java/ru/job4j/collection/SimpleArrayListTest.java
+++ src/test/java/ru/job4j/collection/SimpleArrayListTest.java
@@ -3,6 +3,7 @@ package ru.job4j.collection;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.NoSuchElementException;
@@ -10,6 +11,7 @@ import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.stream.IntStream;
 
+@Disabled("Каркас приложения, тесты будут включены после реализации")
 class SimpleArrayListTest {
 
     private SimpleList<Integer> list;
