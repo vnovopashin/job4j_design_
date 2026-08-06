f450d5aa6aa8452b6743380cd7501835d63c19c7
nova <novopashinva@gmail.com>
2026-08-06 22:30:59 +0700

    9.1. Файлы и директории. Классы Path, Files, Paths [#504936]

---
+++ src/main/java/ru/job4j/io/PathExample.java
@@ -0,0 +1,32 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathExample {
+    public static void main(String[] args) throws IOException {
+        Path directory = Paths.get("path/paths");
+        Files.createDirectories(directory);
+        Path path = Path.of("path/paths/path.txt");
+        Files.createFile(path);
+        System.out.println("Файл/директория существует?: " + Files.exists(path));
+        System.out.println("Это директория?: " + Files.isDirectory(path));
+        System.out.println("Это файл?: " + Files.isRegularFile(path));
+        System.out.println("Имя файла: " + path.getFileName());
+        System.out.println("Путь к файлу абсолютный?: " + path.isAbsolute());
+        System.out.println("Родительская директория файла: " + path.getParent());
+        System.out.println("Абсолютный путь к файлу: " + path.toAbsolutePath());
+        System.out.println("Абсолютный путь к директории: " + directory.toAbsolutePath());
+        System.out.println("Доступен для чтения?: " + Files.isReadable(path));
+        System.out.println("Доступен для записи?: " + Files.isWritable(path));
+        DirectoryStream<Path> paths = Files.newDirectoryStream(directory);
+        paths.forEach(System.out::println);
+        Files.delete(path);
+        Files.delete(directory);
+        Path parentDirectory = Paths.get("path");
+        Files.delete(parentDirectory);
+    }
+}
