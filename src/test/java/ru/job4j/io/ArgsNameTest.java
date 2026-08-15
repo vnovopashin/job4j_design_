3399b308f9c05bc02bf2c2d119ffa06869dfe83d
nova <novopashinva@gmail.com>
2026-08-15 21:27:47 +0700

    13. Именованные аргументы (написал реализацию) [#295518]

--- src/test/java/ru/job4j/io/ArgsNameTest.java
+++ src/test/java/ru/job4j/io/ArgsNameTest.java
@@ -1,12 +1,10 @@
 package ru.job4j.io;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@Disabled("Тесты отключены до реализации методов")
 class ArgsNameTest {
 
     @Test
