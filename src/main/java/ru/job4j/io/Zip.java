cd5e027df0ce3ecc66c29480664637f8d79222e8
nova <novopashinva@gmail.com>
2026-08-18 19:50:02 +0700

    14. Архивировать проект(добавил код в main) [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -2,6 +2,8 @@ package ru.job4j.io;
 
 import java.io.*;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -43,11 +45,25 @@ public class Zip {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Zip zip = new Zip();
         zip.packSingleFile(
                 new File("./pom.xml"),
                 new File("./pom.zip")
         );
+        if (args.length != 3) {
+            throw new IllegalArgumentException("There must be three parameters."
+                    + " For example: -d = C:\\project\\job4j\\ -e = txt -o = project.zip");
+        }
+        ArgsName argsName = ArgsName.of(args);
+        if (!Path.of(argsName.get("d")).toFile().isDirectory()) {
+            throw new Exception("Wrong directory");
+        }
+        List<Path> list = Search.search(Paths.get(argsName.get("d")), p -> !p.toFile().getName().endsWith(argsName.get("e")));
+        List<Path> fileList = new ArrayList<>();
+        for (Path path : list) {
+            fileList.add(path);
+        }
+        zip.packFiles(fileList, new File(argsName.get("o")));
     }
 }
