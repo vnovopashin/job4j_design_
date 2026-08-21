70a8c2d087af7b6b8776cae74d3d47fef360eef7
nova <novopashinva@gmail.com>
2026-08-21 21:39:05 +0700

    16. Кодировка - реализовал методы в классе [#862]

--- src/main/java/ru/job4j/io/ConsoleChat.java
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -1,5 +1,9 @@
 package ru.job4j.io;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConsoleChat {
@@ -15,19 +19,54 @@ public class ConsoleChat {
     }
 
     public void run() {
-
+        List<String> log = new ArrayList<>();
+        List<String> phrases = readPhrases();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String str = br.readLine();
+            while (!OUT.equals(str)) {
+                if (STOP.equals(str)) {
+                    while (!CONTINUE.equals(str)) {
+                        log.add(str);
+                        str = br.readLine();
+                    }
+                }
+                log.add(str);
+                int index = (int) (Math.random() * phrases.size());
+                String answer = phrases.get(index);
+                System.out.println(answer);
+                log.add(answer);
+                str = br.readLine();
+                if (OUT.equals(str)) {
+                    log.add(str);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        saveLog(log);
     }
 
     private List<String> readPhrases() {
-        return null;
+        try {
+            return Files.readAllLines(Path.of(botAnswers));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void saveLog(List<String> log) {
-
+        try (PrintWriter printWriter = new PrintWriter(path)) {
+            for (String s : log) {
+                printWriter.println(s);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void main(String[] args) {
-        ConsoleChat consoleChat = new ConsoleChat("", "");
+        ConsoleChat consoleChat = new ConsoleChat(
+                "./data/logChat.txt", "./data/answers.txt");
         consoleChat.run();
     }
 }
