3f2733287598f098622bc3b5bf91b198f1c23e14
nova <novopashinva@gmail.com>
2026-09-06 22:21:55 +0700

    2. Simple Loggin Facade 4 Java. [#268849]

---
+++ src/main/java/ru/job4j/io/UsageSlf4j.java
@@ -0,0 +1,18 @@
+package ru.job4j.io;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UsageSlf4j {
+
+    private static final Logger LOG =
+            LoggerFactory.getLogger(UsageSlf4j.class.getName());
+
+    public static void main(String[] args) {
+        LOG.trace("trace message");
+        LOG.debug("debug message");
+        LOG.info("info message");
+        LOG.warn("warn message");
+        LOG.error("error message");
+    }
+}
